aba36112d9d221574196dd491746a096765578eb
Angela Luca <angieluca8@gmail.com>
2025-10-28 12:33:20 -0400

    Initial Commit: Added full adder, 4 bit ripple adder, 4 bit ripple adder testbench (Lab 1)

---
+++ Ripple_Carry_Adder_4bit/fa.vhd
@@ -0,0 +1,21 @@
+-- Author: Angela Luca
+-- Description: 1 bit full adder with carry in and carry out
+
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity fa is
+  port (
+    input1    : in  std_logic;
+    input2    : in  std_logic;
+    carry_in  : in  std_logic;
+    sum       : out std_logic;
+    carry_out : out std_logic);
+end fa;
+
+-- DEFINE THE FULL ADDER HERE
+ARCHITECTURE BHV OF fa IS
+BEGIN
+  sum <= input1 XOR input2 XOR carry_in;
+  carry_out <= (input1 AND input2) OR (carry_in AND input1) OR (carry_in AND input2) ;
+END BHV ;
